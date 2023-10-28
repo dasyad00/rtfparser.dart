22f8ac9eb9fc182ca9ea4dcd8a083c788aa01031
Danang Rahmatullah <dasyad00@gmail.com>
2023-10-28 18:39:47 +0200

    Port text converter test

--- test/rtfparser_test.dart
+++ test/rtfparser_test.dart
@@ -1,5 +1,23 @@
+import 'dart:io';
+
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:rtfparser/rtfparser.dart';
 
-void main() {}
+void main() {
+  group("TextConverter", () {
+    test("StringTextConverter", () {
+      final inputRtf = File("test_assets/converter/testTextConversion.rtf")
+          .readAsStringSync();
+      final expectedOutput =
+          File("test_assets/converter/testTextConversion.txt")
+              .readAsStringSync();
+      final source = RtfStringSource(inputRtf);
+      final parser = StandardRtfParser();
+      final listener = StringTextConverter();
+      parser.parse(source, listener);
+      final output = listener.getText();
+      expect(output, equals(expectedOutput));
+    });
+  });
+}
